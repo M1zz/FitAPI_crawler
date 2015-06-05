3066901cbc3c3e38ce6a4647c939745902c50101
seungnamkim <200902094@cs-cnu.org>
2015-06-06 03:01:19 +0900

    googlefit Oauth2.0
    this is for googlefitAuth not fitbitAuth

--- models/api_key_set.js
+++ models/api_key_set.js
@@ -1,10 +1,10 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
-var apiKeySet = new Schema({
+var ApiKeySet = new Schema({
 	jawboneAccessToken: String,
 	fitbitAccessToken: String,
 	googlefitAccessToken: String
 });
 
-module.exports = mongoose.model('api_key_set', apiKeySet);
+module.exports = mongoose.model('api_key_set', ApiKeySet);
