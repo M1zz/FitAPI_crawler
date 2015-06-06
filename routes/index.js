9060a6cb20eaeeff517e397c04b3ac30524e885b
seungnamkim <200902094@cs-cnu.org>
2015-06-06 19:59:48 +0900

    minor changes

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var passport = require('passport');
 var Account = require('../models/account');
-var apiKeySet = require('../models/api_key_set');
+var ApiKeySet = require('../models/api_key_set');
 var https = require('https');
 var router = express.Router();
 
@@ -14,7 +14,7 @@ router.get('/register', function(req, res) {
 });
 
 router.post('/register', function(req, res) {
-	var AKS = new apiKeySet({ jawboneAccessToken: null, fitbitAccessToken: null, googlefitAccessToken: null });
+	var AKS = new ApiKeySet({ jawboneAccessToken: null, fitbitAccessToken: null, googlefitAccessToken: null });
 
 	AKS.save(function( err, data ) {
 		if (err) {
@@ -51,30 +51,33 @@ router.get('/logout', function(req, res) {
 });
 
 router.get('/jawboneAuth', function(req, res) {
-	var token = req.query.code
+	var code		  = req.query.code;
+	var client_id	  = 'UyxeL9V3KSQ';
+	var client_secret = '86c395e53be487a18b283039170f0f31adc068c7';
+	var grant_type	  = 'authorization_code';
 	
-	console.log(token);
+	
+	console.log('receiving code to exchange : ' + code);
 
 	var options = {
 		host: 'jawbone.com',
-		path: '/auth/oauth2/token?client_id=UyxeL9V3KSQ&client_secret=86c395e53be487a18b283039170f0f31adc068c7&grant_type=authorization_code&code='+token
+		path: '/auth/oauth2/token?client_id='+ client_id + '&client_secret='
+				+ client_secret + '&grant_type=' + grant_type + '&code=' + code
 	};
 
 	var jawboneAPIkey = https.get(options, function(Jres) {
 		var res_data = '';
+		
 		Jres.on('data', function(chunk) {
-			console.log('do');
 			res_data += chunk;
 		});
+		
 		Jres.on('end', function() {
-			console.log(res_data);
 			var json = JSON.parse(res_data);
-
-			var jtoken = json.access_token;
 
 			var userAKSPK = req.user.apiKeySetPK;
 
-			apiKeySet.findByIdAndUpdate(userAKSPK, { jawboneAccessToken : res_data }, function(err, found) {
+			ApiKeySet.findByIdAndUpdate(userAKSPK, { jawboneAccessToken : res_data }, function(err, found) {
 				if (err) {
 					console.log(err);
 				} else {
@@ -82,11 +85,54 @@ router.get('/jawboneAuth', function(req, res) {
 				}
 			});
 					
-			res.render('jawboneTokenShow', {user : req.user.username, jtoken : jtoken});
+			res.render('jawboneTokenShow', {user : req.user.username, jtoken : json});
 		});
 	})
 	
 	jawboneAPIkey.on('error', function(e) {
+		console.log("Got error: " + e.message);
+	});
+});
+
+router.get('googlefitAuth', function(res, req) {
+	var code		  = req.query.code;
+	var client_id	  = 'UyxeL9V3KSQ';
+	var client_secret = '86c395e53be487a18b283039170f0f31adc068c7';
+	var grant_type	  = 'authorization_code';
+	
+	console.log('receiving code to exchange : ' + code);
+
+	var options = {
+		host: 'jawbone.com',
+		path: '/auth/oauth2/token?client_id=' + client_id + '&client_secret='
+			   +client_secret + '&grant_type=' + grant_type + '&code=' + code
+	};
+
+	var googlefitAPIkey = https.get(options, function(Gres) {
+		var res_data = '';
+		
+		Gres.on('data', function(chunk) {
+			res_data += chunk;
+		});
+		
+		Gres.on('end', function() {
+			var json = JSON.parse(res_data);
+			
+			var userAKSPK = req.user.apiKeySetPK;
+
+			ApiKeySet.findByIdAndUpdate(userAKSPK, { googlefitAccessToken : res_data }, function(err, found) {
+				if (err) {
+					console.log(err);
+				} else {
+					console.log(found);
+				}
+			});
+					
+			res.render('googlefitTokenShow', {user : req.user.username, gtoken : json});
+		});
+	})
+	
+	googlefitAPIkey.on('error', function(e) {
 		console.log("Got error: " + e.message);
 	});
 });
